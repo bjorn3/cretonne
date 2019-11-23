affc2b6ca232e786996e84a8bd243c480e5af6ab
bjorn3 <bjorn3@users.noreply.github.com>
2019-11-23 18:57:20 +0100

    Use ValueLoc in value_locations

--- cranelift-codegen/src/regalloc/linear_scan.rs
+++ cranelift-codegen/src/regalloc/linear_scan.rs
@@ -5,7 +5,7 @@ use core::cmp::Ordering;
 use cranelift_entity::{SparseMapValue, SecondaryMap};
 
 use crate::cursor::{Cursor, EncCursor};
-use crate::ir::{Function, ProgramOrder, ProgramPoint, Value};
+use crate::ir::{Function, ProgramOrder, ProgramPoint, StackSlotData, StackSlotKind, Value, ValueLoc};
 use crate::isa::{RegUnit, TargetIsa};
 use crate::regalloc::{Context, Liveness, LiveRange};
 
@@ -37,7 +37,7 @@ pub fn run(context: &mut Context, func: &mut Function, isa: &dyn TargetIsa) {
     for reg in isa.allocatable_registers(func).iter(isa.register_info().classes[0]) {
         unused_regs.push(reg);
     }
-    let mut value_locations: SecondaryMap<Value, RegUnit> = SecondaryMap::new();
+    let mut value_locations: SecondaryMap<Value, ValueLoc> = SecondaryMap::new();
 
     let mut live_ranges: Vec<&LiveRange> = context.liveness().ranges().values().collect();
     live_ranges.sort_by(|a, b| func.layout.cmp(a.def(), b.def()));
@@ -46,8 +46,8 @@ pub fn run(context: &mut Context, func: &mut Function, isa: &dyn TargetIsa) {
     for i in live_ranges { // foreach live interval i, in order of increasing start point
         expire_old_intervals(/*context,*/ func, &mut active, &mut unused_regs, &value_locations, i);
         if let Some(reg) = unused_regs.pop() { // if length(active) != R
-            assert!(value_locations[i.key()] == RegUnit::default());
-            value_locations[i.key()] = reg; // register[i] <- a register removed from pool of free registers
+            assert!(value_locations[i.key()] == ValueLoc::Unassigned);
+            value_locations[i.key()] = ValueLoc::Reg(reg); // register[i] <- a register removed from pool of free registers
             insert_active(func, &mut active, i); // add i to active, sorted by increasing end point
         } else {
             spill_at_interval(/*context,*/ func, &mut active, &mut value_locations, i);
@@ -59,7 +59,7 @@ fn expire_old_intervals(
     func: &mut Function,
     active: &mut Vec<&LiveRange>,
     unused_regs: &mut Vec<RegUnit>,
-    value_locations: &SecondaryMap<Value, RegUnit>,
+    value_locations: &SecondaryMap<Value, ValueLoc>,
     i: &LiveRange,
 ) {
     while !active.is_empty() { // foreach interval j in active, in order of increasing end point
@@ -68,24 +68,34 @@ fn expire_old_intervals(
             return;
         }
         active.remove(0); // remove j from active
-        unused_regs.push(value_locations[j.key()]); // add register[j] to pool of free registers
+        unused_regs.push(value_locations[j.key()].unwrap_reg()); // add register[j] to pool of free registers
     }
 }
 
 fn spill_at_interval<'a>(
     func: &mut Function,
     active: &mut Vec<&'a LiveRange>,
-    value_locations: &mut SecondaryMap<Value, RegUnit>,
+    value_locations: &mut SecondaryMap<Value, ValueLoc>,
     i: &'a LiveRange,
 ) {
     let spill = *active.last().expect("Spilling => Register pressure => active.last().is_some()"); // last interval in active
-    if func.layout.cmp(live_range_end(func, spill), live_range_end(func, i)) == Ordering::Greater { // if endpoint[spill] > endpoint[spill]
-        assert!(value_locations[i.key()] == RegUnit::default());
-        value_locations[i.key()] = value_locations[spill.key()]; // register[i] <- register[spill]
-        // location[spill] <- new stack location
+    if func.layout.cmp(live_range_end(func, spill), live_range_end(func, i)) == Ordering::Greater { // if endpoint[spill] > endpoint[i]
+        assert!(value_locations[i.key()] == ValueLoc::Unassigned);
+        value_locations[i.key()] = ValueLoc::Reg(value_locations[spill.key()].unwrap_reg()); // register[i] <- register[spill]
+        /*let slot = func.create_stack_slot(StackSlotData {
+            kind: StackSlotKind::SpillSlot,
+            size: func.dfg.value_type(spill.key()).bytes(),
+            offset: None,
+        });
+        value_locations[spill.key()] = ValueLoc::Stack(slot); // location[spill] <- new stack location*/
         active.pop().unwrap(); // remove spill from active
         insert_active(func, active, i); // add i to active, sorted by increasing end point
     } else {
-        // location[i] <- new stack location
+        /*let slot = func.create_stack_slot(StackSlotData {
+            kind: StackSlotKind::SpillSlot,
+            size: func.dfg.value_type(i.key()).bytes(),
+            offset: None,
+        });
+        value_locations[i.key()] = ValueLoc::Stack(slot); // location[i] <- new stack location*/
     }
 }
