675dd946b25e27e1ddac6fe8667aa56d64038189
bjorn3 <bjorn3@users.noreply.github.com>
2020-02-22 18:33:00 +0100

    [WIP]

--- fuzz/fuzz_reader_parse_test.rs
+++ fuzz/fuzz_reader_parse_test.rs
@@ -1,12 +1,56 @@
-#![no_main]
-#[macro_use]
-extern crate libfuzzer_sys;
-extern crate cranelift_reader;
-use std::str;
+#![cfg_attr(feature = "libfuzzer", no_main)]
 
+#[cfg(not(any(feature = "libfuzzer", feature = "use_afl")))]
+compile_error!("Please enable the libfuzzer of use_afl feature");
+
+#[macro_use]
+#[cfg(feature = "use_afl")]
+extern crate afl;
+
+#[macro_use]
+#[cfg(feature = "libfuzzer")]
+extern crate libfuzzer_sys;
+
+extern crate cranelift_reader;
+extern crate cranelift_codegen;
+#[macro_use]
+extern crate target_lexicon;
+
+use cranelift_codegen::{isa, settings};
+use cranelift_wasm::{translate_module, DummyEnvironment, ReturnMode};
+
+use std::str;
+use std::str::FromStr;
+
+#[cfg(feature = "libfuzzer")]
 fuzz_target!(|data: &[u8]| {
+    fuzz(data);
+});
+
+#[cfg(feature = "use_afl")]
+fn main() {
+    fuzz!(|data: &[u8]| {
+        fuzz(data);
+    });
+}
+
+fn fuzz(data: &[u8]) {
     if let Ok(s) = str::from_utf8(data) {
         let options = cranelift_reader::ParseOptions::default();
-        let _ = cranelift_reader::parse_test(s, options);
+        let test = match cranelift_reader::parse_test(s, options) {
+            Ok(test) => test,
+            _ => return,
+        };
+
+        let flags = settings::Flags::new(settings::builder());
+        let triple = triple!("x86_64");
+        let isa = isa::lookup(triple).unwrap().finish(flags);
+
+        let mut context = cranelift_codegen::Context::new();
+        for (func, _) in test.functions {
+            context.clear();
+            context.func = func;
+            let _ = context.compile(&*isa).unwrap();
+        }
     }
-});
+}
--- fuzz/fuzz_translate_module.rs
+++ fuzz/fuzz_translate_module.rs
@@ -1,7 +1,16 @@
-#![no_main]
+#![cfg_attr(feature = "libfuzzer", no_main)]
+
+#[cfg(not(any(feature = "libfuzzer", feature = "use_afl")))]
+compile_error!("Please enable the libfuzzer of use_afl feature");
 
 #[macro_use]
+#[cfg(feature = "use_afl")]
+extern crate afl;
+
+#[macro_use]
+#[cfg(feature = "libfuzzer")]
 extern crate libfuzzer_sys;
+
 extern crate binaryen;
 extern crate cranelift_codegen;
 extern crate cranelift_wasm;
@@ -12,7 +21,19 @@ use cranelift_codegen::{isa, settings};
 use cranelift_wasm::{translate_module, DummyEnvironment, ReturnMode};
 use std::str::FromStr;
 
+#[cfg(feature = "libfuzzer")]
 fuzz_target!(|data: &[u8]| {
+    fuzz(data);
+});
+
+#[cfg(feature = "use_afl")]
+fn main() {
+    fuzz!(|data: &[u8]| {
+        fuzz(data);
+    });
+}
+
+fn fuzz(data: &[u8]) {
     let binaryen_module = binaryen::tools::translate_to_fuzz_mvp(data);
 
     let wasm = binaryen_module.write();
@@ -22,4 +43,11 @@ fuzz_target!(|data: &[u8]| {
     let isa = isa::lookup(triple).unwrap().finish(flags);
     let mut dummy_environ = DummyEnvironment::new(isa.frontend_config(), ReturnMode::NormalReturns, false);
     translate_module(&wasm, &mut dummy_environ).unwrap();
-});
+
+    let mut context = cranelift_codegen::Context::new();
+    for func in dummy_environ.info.function_bodies.values() {
+        context.clear();
+        context.func = func.clone();
+        context.compile(&*isa).unwrap();
+    }
+}
